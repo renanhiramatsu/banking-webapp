1949eca5529b2253bc143fc5c91359aae6378c26
Renan Hiramatsu <35655046+renanhiramatsu@users.noreply.github.com>
2024-10-26 15:21:08 -0400

    Main page adding account balance, num of accounts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,34 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Renan' };
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Bem-vindo"
+            user={loggedIn?.firstName || 'Convidado'}
+            subtext="Acesse sua conta e gerencie seu dindin."
+          />
+
+          <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+          
+          
+          />
+
+        </header>
+
+      </div>
+    
+    </section>
   )
 }
 
